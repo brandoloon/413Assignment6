422c170ce931d4de627c17142785ea96d33bf835
Brandon Jacobsen <brandonerikjacobsen@gmail.com>
2021-02-17 21:21:28 -0700

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment5.Models
+{
+    public class Book
+    {
+        [Key] // Primary Key for each book
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        [RegularExpression("[0-9]{3}[-][0-9]{10}")] // Require ###-########## format
+        public string ISBN { get; set; }
+        public string Classification { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Models/EFBookStoreRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment5.Models
+{
+    public class EFBookStoreRepository : IBookStoreRepository
+    {
+        private BookStoreDbContext context;
+
+        public EFBookStoreRepository(BookStoreDbContext context)
+        {
+            this.context = context;
+        }
+        public IQueryable<Book> Books => context.Books;
+
+    }
+}
